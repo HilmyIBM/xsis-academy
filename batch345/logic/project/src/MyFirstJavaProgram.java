58e0bd6933f47a6752a76e51b12c0a2be5fe23ca
laqqueta-bootcamp <envyxbot@gmail.com>
2024-12-16 11:54:28 +0700

    Add: First Java Program

---
+++ batch345/logic/project/src/MyFirstJavaProgram.java
@@ -0,0 +1,33 @@
+public class MyFirstJavaProgram {
+
+    /*
+    This is my frist java program
+    This will print 'Hello World' as the output
+     */
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World\n"); // prints hello world
+//
+//        int i = 120501;
+//
+//        switch (i) {
+//            case 10:
+//                System.out.println("Ten");
+//                break;
+//            case 20:
+//                System.out.println("Twenty");
+//                break;
+//            default:
+//                System.out.println("No-match");
+//        }
+//
+//        boolean isMale = true, isMarried = false;
+//        String title = isMale ? "Mr. " : isMarried ? "Mrs. " : "Ms. ";
+//
+//        System.out.println(title + "Zubaid");
+
+        for ( int a = 10, b = 65; a < 999999 & b < 255; a++, b++) {
+            System.out.println((char) b);
+        }
+    }
+}
