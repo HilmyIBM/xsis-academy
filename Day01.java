0227c558d42dab5c488b880bf61f71cdc09fa3ff
Alvito <alvito1010@gmail.com>
2024-12-16 11:48:42 +0700

    day 01

---
+++ Day01.java
@@ -0,0 +1,94 @@
+public class Day01 {
+    public static void main(String []args) {
+        float jari = 28.0F;
+        float sisi = 5.0F;
+        float angka = 10.0F;
+        float pembagi = 4.0F;
+        Float puntung = 100.0F;
+        float harga = 500.0F;
+        float nilai = 85.0F;
+
+        System.out.println("Keliling lingkaran dengan jari-jari " + jari + ": " + kelilingLingkaran(jari));
+
+        System.out.println("Luas lingkaran dengan jari-jari " + jari + ": " + luasLingkaran(jari));
+
+        System.out.println("Keliling Persegi dengan sisi " + sisi + ": " + kelilingPersegi(sisi));
+
+        System.out.println("Luas Persegi dengan sisi " + sisi + ": " + luasPersegi(sisi));
+
+        System.out.println(perhitunganMod(angka, pembagi));
+
+        System.out.println(batangTerkumpul(puntung));
+
+        System.out.println(keuntungan(puntung, harga));
+
+        System.out.println(sisaPuntung(puntung));
+        
+        System.out.println(grade(nilai));
+
+        System.out.println(ganjilgenap(angka));
+
+    }
+
+    public static float kelilingLingkaran(float jari){
+        float ans = 22.0F / 7 * 2 * jari;
+        return ans;
+    }
+
+    public static float luasLingkaran(float jari){
+        float ans = 22.0F / 7 * jari * jari;
+        return ans;
+    }
+
+    public static float kelilingPersegi (float sisi) {
+        float ans = 4.0F * sisi;
+        return ans;
+    }
+
+    public static float luasPersegi (float sisi) {
+        float ans = sisi * sisi;
+        return ans;
+    }
+
+    public static String perhitunganMod (float angka, float pembagi){
+        float ans = angka%pembagi;
+        if (ans == 0.0F) {
+            return "angka(" + angka + ")%(" + pembagi + ") adalah 0";
+        }else {
+            return "angka(" + angka + ")%(" + pembagi + ") bukan 0 melainkan " + ans;
+        }
+    }
+
+    public static Float batangTerkumpul (float puntung) {
+        Float ans = (puntung - puntung%8) / 8;
+        return ans;
+    }
+
+    public static Float keuntungan (float puntung, float harga){
+        float ans = (puntung - puntung%8) / 8 * harga;
+        return ans;
+    }
+
+    public static float sisaPuntung (float puntung){
+        float ans = puntung%8;
+        return ans;
+    }
+
+    public static String grade (float nilai){
+        if (nilai >= 80.0F){
+            return "A";
+        } else if (nilai <80.0F && nilai >= 60.0F){
+            return "B";
+        } else {
+            return "C";
+        }
+    }
+
+    public static String ganjilgenap (float angka){
+        if (angka%2 == 0){
+            return "Genap";
+        } else {
+            return "Ganjil";
+        }
+    }
+}
--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -4,6 +4,31 @@ public class MyFirstJavaProgram {
      */
 
      public static void main(String []args) {
-        System.out.println("Hello World"); //prints Hello World
+        System.out.println("Hello World\n"); //prints Hello World
+
+        int i = 10;
+        switch (i) {
+         case 10:
+            System.out.println("Ten");
+            break;
+         case 20:
+            System.out.println("Twenty");
+            break;
+         default:
+            System.out.println("No match");
+            break;
+        }
+
+        String title= "";
+        boolean isMale = false, isMarried = true;
+
+        title = (isMale ? "Mr" : (isMarried ? "Mrs" : "Ms"));
+        System.out.println(title);
+     }
+
+     public static void forLoop(){
+      for (int i = 10; i < 15; i++){
+         
+      }
      }
 }
