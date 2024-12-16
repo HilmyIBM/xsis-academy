0f86b17091b2656059dc76af92151046d3dbd938
laqqueta-bootcamp <envyxbot@gmail.com>
2024-12-16 17:05:32 +0700

    Modified: Small Refactor

--- batch345/logic/project/src/Day01PR.java
+++ batch345/logic/project/src/Day01PR.java
@@ -1,26 +1,26 @@
 public class Day01PR {
 
-    public static char grade(int nilai) {
+    public static char Grade(int nilai) {
         if (nilai >= 80) return 'A';
         if (nilai >= 60 & nilai < 80) return 'B';
         return 'C';
     }
 
-    public static void oddEven(int n) {
+    public static void EvenOdd(int n) {
         if (n % 2 == 0) System.out.println("Angka " + n + " adalah genap" );
         else System.out.println("Angka " + n + " adalah ganjil");
     }
 
     public static void main(String[] args) {
         // No 1.
-        int nilai = 60;
-        char grade = grade(nilai);
+        int nilai = 59;
+        char grade = Grade(nilai);
         System.out.println("Dengan Nilai " + nilai + ", Grade yang didapat adalah " + grade);
         System.out.println();
 
         // No 2.
         int n = 19;
-        oddEven(n);
+        EvenOdd(n);
         System.out.println();
     }
 }
