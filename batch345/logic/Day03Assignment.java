e53aa47c60b8f18966b258759404be257b31d2cf
axlrxlr <axel.rham.rus@gmail.com>
2024-12-17 21:19:33 +0700

    Day 03 Soal 1 ok

--- batch345/logic/Day03Assignment.java
+++ batch345/logic/Day03Assignment.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day03Assignment {
   public static void main(String[] args) {
-    
+    SoalPertama();
   }
 
   public static void SoalPertama(){
@@ -11,10 +10,27 @@ public class Day03Assignment {
     System.out.println("Pohon Faktor");
     System.out.print("Input >> ");
     int number = Integer.parseInt(input.nextLine());
+    input.close();
     PohonFaktor(number);
+    
   }
 
   public static void PohonFaktor(int x){
-    for (i)
+    int divider = 0;
+    for(int i = 2; i<=x; i++){
+      if (x % i == 0) {
+        divider = i;
+        break;
+      }
+    }
+    int result = x / divider;
+    System.out.println(x + " / " + divider + " = " + result);
+    if (result != 1) {
+      PohonFaktor(result);
+    }
+  }
+
+  public static void SoalKedua(){
+    
   }
 }
