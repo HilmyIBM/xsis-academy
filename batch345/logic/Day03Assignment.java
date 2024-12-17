8be4633a3d6f48a8c79c6d3bde9dd203421fa38e
axlrxlr <axel.rham.rus@gmail.com>
2024-12-17 16:42:23 +0700

    Added day 03 assignment file

---
+++ batch345/logic/Day03Assignment.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day03Assignment {
+  public static void main(String[] args) {
+    
+  }
+
+  public static void SoalPertama(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Pohon Faktor");
+    System.out.print("Input >> ");
+    int number = Integer.parseInt(input.nextLine());
+    PohonFaktor(number);
+  }
+
+  public static void PohonFaktor(int x){
+    for (i)
+  }
+}
