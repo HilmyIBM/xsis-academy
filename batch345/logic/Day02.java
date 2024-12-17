789ee1a8dcf6d8212f87a991803bffee7a93146a
johannessitinjak <johannes.7tin@gmail.com>
2024-12-18 01:27:32 +0700

    melengkapi Quiz dan PR

--- batch345/logic/Day02.java
+++ batch345/logic/Day02.java
@@ -6,37 +6,64 @@ public class Day02 {
     }
 
     public static void pilihSoal(){
-        System.out.print("Soal No. Berapa yang akan dikerjakan : ");
         Scanner input = new Scanner(System.in);     
-        int noSoal = input.nextInt();
-        switch (noSoal) {
-            case 1:
-                System.out.println(gradeNilai());
+        int noSoal;
+
+        while (true) {
+            System.out.print("Soal No. Berapa yang akan dikerjakan (0 untuk keluar dari program) : ");
+            noSoal = input.nextInt();
+            
+            if(noSoal == 0) {
+                System.out.println("Terima Kasih");
                 break;
-            case 2:
-                System.out.println(pointPulsa());
-                break;
-            case 3:
-                System.out.println(grabFood());
-                break;
-            case 4:
-                System.out.println(ongkirSopi());
-                break;
-            case 5:
-                System.out.println(generasi());
-                break;
-            default:
-                break;
+            }
+
+            
+            switch (noSoal) {
+                case 1:
+                    System.out.print("\nMasukkan Nilai : ");
+                    int nilai = input.nextInt();
+                    System.out.println(gradeNilai(nilai));
+                    break;
+                case 2:
+                    System.out.print("\nMasukkan Jumlah Pulsa : ");
+                    int pulsa = input.nextInt();
+                    System.out.println(pointPulsa(pulsa));
+                    break;
+                case 3:
+                    System.out.println("Belanja : ");
+                    double belanja = input.nextDouble();
+                    System.out.println("Jarak : ");
+                    double jarak = input.nextDouble();
+                    input.nextLine();
+                    System.out.println("Masukkan Kode Promo : ");
+                    String promo = input.nextLine();
+                    System.out.println(grabFood(belanja, jarak, promo));
+                    break;
+                case 4:
+                    System.out.println("Belanja : ");
+                    double totalBelanja = input.nextDouble();
+                    System.out.println("Ongkos Kirim : ");
+                    double ongkos = input.nextDouble();
+                    System.out.println(ongkirSopi(totalBelanja, ongkos));
+                    break;
+                case 5:
+                    System.out.print("Masukkan Nama Anda : ");
+                    String nama = input.nextLine();
+                    System.out.print("Tahun Berapa Anda Lahir : ");
+                    int tahun = input.nextInt();
+                    System.out.println(generasi(nama, tahun));
+                    break;
+                default:
+                    System.out.println("Nomor tidak ada!");
+                    break;
+            }
         }
+        input.close();
     }
 
-
-
     // No 1
-    public static String gradeNilai(){
-        Scanner input = new Scanner(System.in);     
-        System.out.print("Masukkan Nilai : ");
-        int nilai = input.nextInt();
+    public static String gradeNilai(int nilai){ 
         if (nilai >= 90 && nilai <= 100) {
             return "Grade: A\n";
         }else if (nilai >= 70 && nilai < 89) {
@@ -48,12 +75,8 @@ public class Day02 {
         }
     }
 
-
     // No 2
-    public static String pointPulsa(){
-        Scanner input = new Scanner(System.in);     
-        System.out.print("Masukkan Jumlah Pulsa : ");
-        int nilai = input.nextInt();
+    public static String pointPulsa(int nilai){     
         if (nilai >= 100000) {
             return "Pulsa\t:"+ nilai + "\n Point\t: 800";
         }else if (nilai >= 50000) {
@@ -68,18 +91,8 @@ public class Day02 {
     }
 
     // No 3
-    public static String grabFood(){
-        Scanner input = new Scanner(System.in);     
-        System.out.println("Belanja : ");
-        double belanja = input.nextDouble();
-        System.out.println("Jarak : ");
-        double jarak = input.nextDouble();
-        input.nextLine();
-        System.out.println("Masukkan Kode Promo : ");
-        String promo = input.nextLine();
+    public static String grabFood(double belanja, double jarak, String promo){
         String kodePromo = "JKTOVO";
-        System.out.println(promo);
-
         if (belanja >= 30000) {
             if (promo.equals(kodePromo)) {
                 double total = belanja -  belanja/10*4 + jarak*1000;
@@ -94,13 +107,7 @@ public class Day02 {
     }
 
     // No 4
-    public static String ongkirSopi(){
-        Scanner input = new Scanner(System.in);     
-        System.out.println("Belanja : ");
-        double belanja = input.nextDouble();
-        System.out.println("Ongkos Kirim : ");
-        double ongkos = input.nextDouble();
-
+    public static String ongkirSopi(double belanja, double ongkos){
         double diskonOngkir;
         double diskonBelanja;
         double totalBelanja;
@@ -125,13 +132,7 @@ public class Day02 {
 
 
     // No 5
-    public static String generasi(){
-        Scanner input = new Scanner(System.in);     
-        System.out.print("Masukkan Nama Anda : ");
-        String nama = input.nextLine();
-        System.out.print("Tahun Berapa Anda Lahir : ");
-        int tahun = input.nextInt();
-
+    public static String generasi(String nama, int tahun){
         String namaGenerasi = "";
         if (tahun >= 1944 && tahun <= 1964) {
             namaGenerasi = "Baby boomer";
--- batch345/logic/PRDay02.java
+++ batch345/logic/PRDay02.java
@@ -6,26 +6,80 @@ public class PRDay02 {
     }
 
     public static void pilihSoal(){
-        System.out.print("Soal No. Berapa yang akan dikerjakan : ");
         Scanner input = new Scanner(System.in);     
-        int noSoal = input.nextInt();
-        switch (noSoal) {
-            case 1:
-                System.out.println(hitungPajak())
-                System.out.print("Nama : \t\t");
+        int noSoal;
+
+        while (true) {
+            System.out.print("Soal No. Berapa yang akan dikerjakan (0 untuk keluar dari program) : ");
+            noSoal = input.nextInt();
+            
+            if(noSoal == 0) {
+                System.out.println("Terima Kasih");
                 break;
-            case 2:
-                System.out.println(pointPulsa());
-                break;
-            case 3:
-                System.out.println(grabFood());
-                break;
-            default:
-                break;
+            }
+
+            
+            switch (noSoal) {
+                case 1:
+                    System.out.print("\nMasukkan Nama : ");
+                    input.nextLine();
+                    String nama = input.nextLine();
+                    System.out.print("Jumlah Tunjangan: ");
+                    double tunjangan = input.nextDouble();
+                    System.out.print("Jumlah Gaji Pokok : ");
+                    double gapok = input.nextDouble();
+                    System.out.print("Banyak Bulan : ");
+                    double banyakBulan = input.nextDouble();
+                    hitungGaji(nama, tunjangan, gapok, banyakBulan);
+                    break;
+                case 2:
+                    System.out.print("\nMasukkan Berat Badan Anda: ");
+                    double berat = input.nextDouble();
+                    System.out.print("Masukkan Tinggi Badan Anda : ");
+                    double tinggi = input.nextDouble();
+                    kurusGemuk(berat, tinggi);
+                    break;
+                default:
+                    System.out.println("Nomor tidak ada!");
+                    break;
+            }
         }
+        input.close();
     }
 
-    public static String hitungPajak();
+    public static void hitungGaji(String nama, double tunjangan, double gapok, double banyakBulan){ 
+        double pajak, bpjs, gajiBulanan;
 
+        System.out.println("Karyawan atas nama "+ nama + " slip gaji sebagai berikut :");
+        if ((tunjangan+gapok) > 10000000) {
+            pajak = (tunjangan+gapok) * 0.15;
+        } else if ((tunjangan+gapok) > 5000000)  {
+            pajak = (tunjangan+gapok) * 0.1;
+        }else{
+            pajak = (tunjangan+gapok) * 0.05;
+        }
+        bpjs = (tunjangan+gapok) * 0.03;
+        gajiBulanan = (tunjangan+gapok)-(pajak + bpjs);
 
+        System.out.println("Pajak :\t\t\tRp." + pajak);
+        System.out.println("BPJS :\t\t\tRp." + bpjs);
+        System.out.println("Gaji/bln :\t\tRp." + gajiBulanan);
+        System.out.println("Total Gaji :\t\tRp." + gajiBulanan*banyakBulan);
+    }
+
+    public static void kurusGemuk(double berat, double tinggi){ 
+        String badan;
+        tinggi /= 100;
+        double bmi = berat/tinggi*tinggi;
+        if (bmi >= 25) {
+            badan = "Gemuk\n";
+        } else if (bmi >= 18.5) {
+            badan = "Langsing\n";
+        }else{
+            badan = "Kurus\n";
+        }
+
+        System.out.println("\nNilai BMI Anda Adalah " + bmi);
+        System.out.println("Anda Termasuk Berbadan " + badan);
+    }
 }
