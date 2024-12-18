edf1a8d0ecabe17c16716f29113f6d8a9ad301f5
hasnan99 <hasnanhabib8@gmail.com>
2024-12-18 21:22:51 +0700

    Pr Day 04

--- batch345/logic/Day04.java
+++ batch345/logic/Day04.java
@@ -1,57 +1,107 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day04 {
     static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-  
-        /* String kalimat = input.nextLine();
-        kata(kalimat);
+        /*
+         * System.out.println("Masukkan kalimat : ");
+         * String kalimat = input.nextLine();
+         * kata(kalimat);
+         * 
+         * System.out.println("===============");
+         * System.out.println("Masukkan kalimat : ");
+         * String upper = input.nextLine();
+         * upper_lower(upper);
+         * 
+         * System.out.println("===============");
+         * System.out.println("Masukkan kalimat : ");
+         * String mid = input.nextLine();
+         * mid_bintang(mid);
+         * 
+         * System.out.println("===============");
+         * System.out.println("Masukkan kalimat : ");
+         * String awal = input.nextLine();
+         * awal_bintang(awal);
+         */
 
-        String upper=input.nextLine();
-        upper_lower(upper); */
-
-        String mid=input.nextLine();
-        mid_bintang(mid);
+        System.out.println("===============");
+        System.out.println("Masukkan kalimat : ");
+        String kata_3 = input.nextLine();
+        kata_3(kata_3);
     }
 
-    public static void kata(String n){
-        String [] parts=n.split(" ");
-        for(int i=0;i<parts.length;i++){
-            System.out.println("Kata "+(i+1) +" "+ parts[i]);
+    public static void kata(String n) {
+        String[] parts = n.split(" ");
+        for (int i = 0; i < parts.length; i++) {
+            System.out.println("Kata " + (i + 1) + " " + parts[i]);
         }
-        System.out.println("Total Kata adalah : "+parts.length);
+        System.out.println("Total Kata adalah : " + parts.length);
     }
 
-    public static void upper_lower(String n){
-        int u=0;
-        int kapital=0;
-        int j=n.length();
-        for(int i =0;i<j;i++){
-            if (n.charAt(i)=='U' || n.charAt(i)=='u') {
+    public static void upper_lower(String n) {
+        int u = 0;
+        int kapital = 0;
+        int j = n.length();
+        for (int i = 0; i < j; i++) {
+            if (n.charAt(i) == 'U' || n.charAt(i) == 'u') {
                 u++;
-            }if(Character.isUpperCase(n.charAt(i))){
+            }
+            if (Character.isUpperCase(n.charAt(i))) {
                 kapital++;
             }
         }
-        System.out.println("Huruf u ada : "+ u);
-        System.out.println("Huruf Kapital ada : "+ kapital);
+        System.out.println("Huruf u ada : " + u);
+        System.out.println("Huruf Kapital ada : " + kapital);
     }
 
-    public static void mid_bintang(String n){
-        String[] kata=n.split(" ");
-        ArrayList<String> hasil=new ArrayList<String>();
-        for(int i=0;i<kata.length;i++){
-            if(i==1 && i<=kata.length-1){
-                hasil.add("*");
-            }else{
-                hasil.add(kata[i]);
+    public static void mid_bintang(String n) {
+        String[] kata = n.split(" ");
+        String hasil = "";
+        for (String kata_kata : kata) {
+            for (int i = 0; i < kata_kata.length(); i++) {
+                if (i == 0 || i == kata_kata.length() - 1) {
+                    hasil += kata_kata.charAt(i);
+                } else {
+                    hasil += "*";
+                }
             }
-            hasil.add(" ");
+            hasil += " ";
         }
-        
+        System.out.println(hasil);
+    }
 
+    public static void awal_bintang(String n) {
+        String[] kata = n.split(" ");
+        String hasil = "";
+        for (String kata_kata : kata) {
+            for (int i = 0; i < kata_kata.length(); i++) {
+                if (i == 0 || i == kata_kata.length() - 1) {
+                    hasil += "*";
+                } else {
+                    hasil += kata_kata.charAt(i);
+                }
+            }
+            hasil += " ";
+        }
+        System.out.println(hasil);
+    }
+
+    public static void kata_3(String n) {
+        String[] kata = n.split(" ");
+        String hasil = "";
+        for (String kata_kata : kata) {
+            if (kata_kata.length() < 3) {
+                hasil += kata_kata;
+            } else {
+                hasil += kata_kata.substring(kata_kata.length() - 3);
+            }
+            hasil += " ";
+        }
+        System.out.println(hasil);
     }
 }
---
+++ batch345/logic/PrDay04.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrDay04 {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String input_polindrom = input.nextLine();
+        polindrom(input_polindrom);
+
+        System.out.println("===============");
+        System.out.println("Masukkan uang : ");
+        int uang = input.nextInt();
+        input.nextLine();
+        System.out.println("Masukkan harga baju : ");
+        String baju = input.nextLine();
+        System.out.println("Masukkan harga celana : ");
+        String celana = input.nextLine();
+        lebaran(uang, baju, celana);
+    }
+
+    public static void polindrom(String n) {
+        String hasil = new StringBuilder(n).reverse().toString();
+        if (n.equals(hasil)) {
+            System.out.println("Benar");
+        } else {
+            System.out.println("Salah");
+        }
+    }
+
+    public static void lebaran(int uang, String baju, String celana) {
+        int[] harga_baju = Arrays.stream(baju.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] harga_celana = Arrays.stream(celana.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] total = new int[harga_baju.length];
+        Integer min = 0;
+        for (int i = 0; i < harga_baju.length; i++) {
+            total[i] = harga_baju[i] + harga_celana[i];
+            if (total[i] <= uang && total[i] > min) {
+                min = total[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+}
