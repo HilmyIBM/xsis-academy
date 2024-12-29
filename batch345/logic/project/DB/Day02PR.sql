924b57e3d65da0147fbd7a5393418769c8c8420b
laqqueta-ubuntu <envyxbot@gmail.com>
2024-12-29 20:11:48 +0700

    aaa

--- batch345/logic/project/DB/Day02PR.sql
+++ batch345/logic/project/DB/Day02PR.sql
@@ -1,12 +1,15 @@
--- CREATE DATABASE "DB_Entertainer";
-
+CREATE DATABASE "DB_Entertainer";
+--
 CREATE TABLE artis (
     kd_artis char(4) PRIMARY KEY not null,
     nm_artis varchar(64) not null,
     jk varchar(10) not null,
     bayaran BIGINT not null,
     award int2 not null,
-    negara varchar(3)
+    negara varchar(3),
+    CONSTRAINT fk_negara
+                   FOREIGN KEY (negara)
+                   REFERENCES negara(kd_negara)
 );
 
 CREATE TABLE produser (
@@ -68,8 +71,6 @@ VALUES
     ('P004', 'PARKIT', 'TIDAK'),
     ('P005', 'PARAMOUNT PICTURE', 'YA');
 
-DROP TABLE artis;
-
 INSERT INTO artis (kd_artis, nm_artis, jk, bayaran, award, negara)
 VALUES
     ('A001', 'ROBERT DOWNEY JR', 'PRIA', 5000000000, 2, 'AS'),
@@ -77,3 +78,24 @@ VALUES
     ('A003', 'JACKIE CHAN', 'PRIA', 200000000, 7, 'HK'),
     ('A004', 'JOE TASLIM', 'PRIA', 350000000, 1, 'ID'),
     ('A005', 'CHELSEA ISLAN', 'WANITA', 300000000, 0, 'ID');
+
+INSERT INTO public.film (kd_film, nm_film, genre, artis, produser, pendapatan, nominasi)
+VALUES
+    ('P001', 'IRON MAN', 'G001', 'A001', 'P001', 2000000000, 3),
+    ('P002', 'IRON MAN 2', 'G001', 'A001', 'P001', 1800000000, 2),
+    ('P003', 'IRON MAN 3', 'G001', 'A001', 'P001', 1200000000, 0),
+    ('P004', 'AVANGER : CIVIL WAR', 'G001', 'A001', 'P001', 2000000000, 1),
+    ('P005', 'SPIDERMAN : HOME COMING', 'G001', 'A001', 'P001', 1200000000, 0),
+    ('P005', 'SPIDERMAN : HOME COMING', 'G001', 'A001', 'P001', 1200000000, 0),
+    ('P007', 'FAST & FURIOUS', 'G001', 'A004', 'P005', 830000000, 2),
+    ('P008', 'HABIBIE DAN AINUN', 'G004', 'A005', 'P003', 670000000, 4),
+    ('P009', 'POLICE STORY', 'G001', 'A003', 'P002', 700000000, 3),
+    ('P010', 'POLICE STORY 2', 'G001', 'A003', 'P002', 710000000, 1),
+    ('P011', 'POLICE STORY 3', 'G001', 'A003', 'P002', 615000000, 0),
+    ('P012', 'RUSH HOUR', 'G003', 'A003', 'P005', 695000000, 2),
+    ('P013', 'KUNGFU PANDA', 'G003', 'A003', 'P005', 923000000, 5);
+
+
+------------------- Task -------------------
+
+-- NO 1
