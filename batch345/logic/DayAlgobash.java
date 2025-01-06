27a1c3b5b9af7eaab274e2ad88593b05c2662959
Jesko701 <IRZHunterBlood@yahoo.com>
2025-01-07 06:00:05 +0700

    add more additional

--- batch345/logic/DayAlgobash.java
+++ batch345/logic/DayAlgobash.java
@@ -27,4 +27,48 @@ public class DayAlgobash {
 
         System.out.println(result.toString());
     }
+
+    public class ReverseStringCompare {
+        public static void main(String[] args) {
+            String original = "madam";
+            char[] originalArray = original.toCharArray();
+            char[] reversedArray = new char[originalArray.length];
+    
+            // Reverse the string into another array
+            for (int i = 0; i < originalArray.length; i++) {
+                reversedArray[i] = originalArray[originalArray.length - 1 - i];
+            }
+    
+            // Print original and reversed arrays
+            System.out.println("Original Array: ");
+            printArray(originalArray);
+    
+            System.out.println("Reversed Array: ");
+            printArray(reversedArray);
+    
+            // Compare the original and reversed arrays
+            boolean areEqual = true;
+            for (int i = 0; i < originalArray.length; i++) {
+                if (originalArray[i] != reversedArray[i]) {
+                    areEqual = false;
+                    break;
+                }
+            }
+    
+            if (areEqual) {
+                System.out.println("The original and reversed arrays are identical.");
+            } else {
+                System.out.println("The original and reversed arrays are different.");
+            }
+        }
+    
+        // Utility method to print a character array
+        public static void printArray(char[] array) {
+            for (char c : array) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+    
 }
