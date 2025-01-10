324f6d04481aba3a30c200baa16ae736ebf65dd4
Sienszz <albertsilva381@gmail.com>
2025-01-10 17:18:41 +0700

    setup js-jquery-bootstrap

--- batch345/technology/demo/src/main/java/com/xsis/demo/controller/DemoController.java
+++ batch345/technology/demo/src/main/java/com/xsis/demo/controller/DemoController.java
@@ -1,13 +1,8 @@
 package com.xsis.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
-// import ch.qos.logback.core.model.Model;  
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
---
+++ batch345/technology/demo/src/main/resources/static/home.js
@@ -0,0 +1,7 @@
+function halo(){
+    if(confirm("please press a button") == true){
+        alert("halo")
+    }else{
+        alert("bye-bye")
+    }
+}
