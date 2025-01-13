4c3b0f59b444763add374706dfe1b1a969cd1695
Sienszz <albertsilva381@gmail.com>
2025-01-13 17:26:31 +0700

    posProject

--- batch345/technology/demo/src/main/java/com/xsis/demo/controller/DemoController.java
+++ batch345/technology/demo/src/main/java/com/xsis/demo/controller/DemoController.java
@@ -2,16 +2,31 @@ package com.xsis.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
 
 
 @Controller
 public class DemoController {
-    @GetMapping("/")
-    public String home(Model model){
-        model.addAttribute("message", "Welcome to Spring MVC");
-        return "home";
+    // @GetMapping("/")
+    // public String home(Model model){
+    //     model.addAttribute("message", "Welcome to Spring MVC");
+    //     return "home";
+    // }
+     @GetMapping("/")
+    public ModelAndView home(){
+        return new ModelAndView("/home");
     }
+    @GetMapping("/about")
+    public ModelAndView about(){
+        return new ModelAndView("/about");
+    }
+    // @GetMapping("/about")
+    // public String about(Model model) {
+    //     model.addAttribute("message", "Welcome to Spring MVC");
+    //     return "about";
+    // }
+    
     // @PostMapping("/greet")
     // public String greetUser(@RequestParam("name") String name, Model model) {
     //     //TODO: process POST request
---
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.xsis.bc345.fe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("/index");
+    }
+    @GetMapping("/about")
+    public ModelAndView about() {
+        return new ModelAndView("/about");
+    }
+}
