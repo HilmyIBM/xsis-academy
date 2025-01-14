e2b26b5d66791f3a9e006616f4477f9ebdf6fbeb
Imi Comp <imicomputer@users.noreply.github.com>
2025-01-14 11:30:18 +0700

    + Add new  category Modal PopUp

--- batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
@@ -2,15 +2,27 @@ package com.xsis.bc345.fe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
     
-    @GetMapping("/category")
+    @GetMapping("")
     public ModelAndView index() {
         ModelAndView view = new ModelAndView("/category/index");
 
         return view;
     }
+
+    @GetMapping("/add")
+    public ModelAndView add() {
+        ModelAndView view = new ModelAndView("/category/add");
+        view.addObject("title", "Add New Category");
+
+        return view;
+    }
+    
 }
