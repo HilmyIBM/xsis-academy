3d906fc9fb0c3289345d1a1e7ac10b5eacb40a8b
zollycp87 <zollycitra87@gmail.com>
2025-01-14 09:19:49 +0700

    delete fontawesome

--- batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/ProductController.java
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.xsis.bc345.fe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
