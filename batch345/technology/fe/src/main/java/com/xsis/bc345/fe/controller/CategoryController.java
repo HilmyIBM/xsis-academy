1b1915dbac737cb75f4b3cc868dd4efd2bf9eead
Imi Comp <imicomputer@users.noreply.github.com>
2025-01-14 15:57:48 +0700

    + Category Add Form + Category Edit Form

--- batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
@@ -2,8 +2,14 @@ package com.xsis.bc345.fe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.xsis.bc345.fe.models.Category;
+
 
 
 @Controller
@@ -11,18 +17,50 @@ import org.springframework.web.servlet.ModelAndView;
 public class CategoryController {
     
     @GetMapping("")
-    public ModelAndView index() {
+    ModelAndView index() {
         ModelAndView view = new ModelAndView("/category/index");
 
         return view;
     }
 
     @GetMapping("/add")
-    public ModelAndView add() {
+    ModelAndView add() {
         ModelAndView view = new ModelAndView("/category/add");
         view.addObject("title", "Add New Category");
+        view.addObject("nama", "Nama Saya Budi");
 
         return view;
     }
     
+    @PostMapping("/save")
+    String save(@ModelAttribute Category category) {
+        try {
+            if (category.getName() == "") {
+                return "Gagal";
+            }
+            else {
+                return "Sukses";
+            }
+        }
+        catch(Exception e) {
+            return "Gagal";
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    ModelAndView edit(@PathVariable long id) {
+        ModelAndView view = new ModelAndView("/category/edit");
+
+        // Get Category data by requested Category ID
+        Category category = new Category();
+        category.setId(id);
+        category.setName("Makanan");
+        category.setDescription("Kategory Makanan");
+        category.setDeleted(false);
+
+        view.addObject("title", "Edit Category");
+        view.addObject("category", category);
+
+        return view;
+    }
 }
---
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/models/Category.java
@@ -0,0 +1,66 @@
+package com.xsis.bc345.fe.models;
+
+import java.time.LocalDateTime;
+
+public class Category {
+    private long id;
+    private String name;
+    private String description;
+
+    private boolean deleted;
+
+    private int createBy;
+    private LocalDateTime createDate;
+    private int updateBy;
+    private LocalDateTime updateDate;    
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isDeleted() {
+        return deleted;
+    }
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+    public int getCreateBy() {
+        return createBy;
+    }
+    public void setCreateBy(int createBy) {
+        this.createBy = createBy;
+    }
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+    public int getUpdateBy() {
+        return updateBy;
+    }
+    public void setUpdateBy(int updateBy) {
+        this.updateBy = updateBy;
+    }
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
