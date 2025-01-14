0aa69d911c21a4e609594a5bbc8d89476d7ea5a3
Imi Comp <imicomputer@users.noreply.github.com>
2025-01-14 17:17:05 +0700

    + Category Edit & Update

--- batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CategoryController.java
@@ -1,5 +1,9 @@
 package com.xsis.bc345.fe.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,10 +19,31 @@ import com.xsis.bc345.fe.models.Category;
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
+    private List<Category> data = new ArrayList<Category>();
+
+    CategoryController() {
+        data.add(new Category());
+        data.get(0).setId(1);
+        data.get(0).setName("Makanan");
+        data.get(0).setDescription("Kategori Makanan");
+        data.get(0).setDeleted(false);
+        data.get(0).setCreateBy(1);
+        data.get(0).setCreateDate(LocalDateTime.now());
+
+        data.add(new Category());
+        data.get(1).setId(2);
+        data.get(1).setName("Obat");
+        data.get(1).setDescription("Kategori Obat-obatan");
+        data.get(1).setDeleted(false);
+        data.get(1).setCreateBy(1);
+        data.get(1).setCreateDate(LocalDateTime.now());
+    }
     
     @GetMapping("")
     ModelAndView index() {
         ModelAndView view = new ModelAndView("/category/index");
+
+        view.addObject("category", data);
 
         return view;
     }
@@ -50,17 +75,27 @@ public class CategoryController {
     @GetMapping("/edit/{id}")
     ModelAndView edit(@PathVariable long id) {
         ModelAndView view = new ModelAndView("/category/edit");
-
         // Get Category data by requested Category ID
-        Category category = new Category();
-        category.setId(id);
-        category.setName("Makanan");
-        category.setDescription("Kategory Makanan");
-        category.setDeleted(false);
+        Category category =  data.get((int)id-1);
 
         view.addObject("title", "Edit Category");
         view.addObject("category", category);
 
         return view;
+    }
+
+    @PostMapping("/update")
+    String update(@ModelAttribute Category category) {
+        try {
+            if (category.getName() == "") {
+                return "Gagal";
+            }
+            else {
+                return "Sukses";
+            }
+        }
+        catch(Exception e) {
+            return "Gagal";
+        }
     }
 }
