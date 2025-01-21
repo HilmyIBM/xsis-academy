98b5b8d8c0be4180e5719ba7b52fe8588521dcb8
laqqueta-bootcamp <envyxbot@gmail.com>
2025-01-21 16:16:16 +0700

    Modify: + Add better try catch - Remove unused service

--- batch345/technology/be/src/main/java/com/xsis/bc345/be/category/CategoryService.java
+++ batch345/technology/be/src/main/java/com/xsis/bc345/be/category/CategoryService.java
@@ -1,22 +1,26 @@
 package com.xsis.bc345.be.category;
 
+import com.xsis.bc345.be.variant.VariantRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryService {
 
+    private static final Logger log = LoggerFactory.getLogger(CategoryService.class);
     private final CategoryRepository categoryRepository;
+    private final VariantRepository variantRepository;
 
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
+    public CategoryService(CategoryRepository categoryRepository, VariantRepository variantRepository) {
         this.categoryRepository = categoryRepository;
+        this.variantRepository = variantRepository;
     }
 
     public List<CategoryModel> getAll() {
@@ -25,67 +29,85 @@ public class CategoryService {
 
             return data.orElse(List.of());
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            throw new RuntimeException(e.getMessage(), e.getCause());
         }
     }
 
-    public Optional<CategoryModel> getById(int id, boolean deleted) {
-        return categoryRepository
-                .findByIdAndDeleted(id, deleted);
-    }
+    public CategoryModel getById(int id, boolean deleted) {
+        try {
+            var data = categoryRepository
+                    .findByIdAndDeleted(id, deleted);
 
-    public Optional<List<CategoryModel>> getByNameOrDescription(String filter) {
-        return categoryRepository
-                .findAllByCategoryNameLikeIgnoreCaseOrDescriptionLikeIgnoreCaseAndDeleted(filter, filter, false);
-    }
+            if (data.isEmpty())
+                throw new EntityNotFoundException("Category with id %s doesnt exists or already deleted".formatted(id));
 
-    public Optional<List<CategoryModel>> getByName(String categoryName) {
-        return categoryRepository.
-                findAllByCategoryNameLikeIgnoreCase(categoryName);
-    }
-
-    public Optional<List<CategoryModel>> getByDesc(String description) {
-        return categoryRepository.
-                findAllByDescriptionLikeIgnoreCase(description);
+            return data.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e.getCause());
+        }
     }
 
     public CategoryModel createCategory(CategoryModel categoryModel) {
         try {
             return categoryRepository.save(categoryModel);
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            throw new RuntimeException(e.getMessage());
         }
     }
 
     public CategoryModel updateCategory(CategoryModel categoryModel) {
-        var data = categoryRepository.findByIdAndDeleted(categoryModel.getId(), false);
+        try {
+            var data = categoryRepository.findByIdAndDeleted(categoryModel.getId(), false);
 
-        if (data.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id %s doesn't exists".formatted(categoryModel.getId()));
+            if (data.isEmpty())
+                throw new EntityNotFoundException("Category with id %s doesn't exists or already deleted".formatted(categoryModel.getId()));
 
-        var existingData = data.get();
+            var existingData = data.get();
 
-        categoryModel.setCreateBy(existingData.getCreateBy());
-        categoryModel.setCreateDate(existingData.getCreateDate());
-        categoryModel.setUpdateDate(LocalDateTime.now());
+            categoryModel.setCreateBy(existingData.getCreateBy());
+            categoryModel.setCreateDate(existingData.getCreateDate());
+            categoryModel.setUpdateDate(LocalDateTime.now());
 
-        return categoryRepository.save(categoryModel);
+            return categoryRepository.save(categoryModel);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e.getCause());
+        }
     }
 
-    public void deleteCategory(CategoryModel model) {
-        var data = categoryRepository.findByIdAndDeleted(model.getId(), false);
+    public CategoryModel deleteCategory(CategoryModel model) {
+        try {
+            var data = categoryRepository.findByIdAndDeleted(model.getId(), false);
+//            var count = variantRepository.countVariantModelByCategory_Id(model.getId());
 
-        if (data.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id %s doesn't exists".formatted(model.getId()));
+            if (data.isEmpty())
+                throw new EntityNotFoundException("Category with id %s doesn't exists or already deleted".formatted(model.getId()));
 
-        var existingData = data.get();
+            var existingData = data.get();
 
-        existingData.setUpdateBy(model.getUpdateBy());
-        existingData.setUpdateDate(LocalDateTime.now());
-        existingData.setDeleted(true);
+            existingData.setUpdateBy(model.getUpdateBy());
+            existingData.setUpdateDate(LocalDateTime.now());
+            existingData.setDeleted(true);
 
-        categoryRepository.save(existingData);
+            return categoryRepository.save(existingData);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e.getCause());
+        }
     }
+
+//    public Optional<List<CategoryModel>> getByNameOrDescription(String filter) {
+//        return categoryRepository
+//                .findAllByCategoryNameLikeIgnoreCaseOrDescriptionLikeIgnoreCaseAndDeleted(filter, filter, false);
+//    }
+//
+//    public Optional<List<CategoryModel>> getByName(String categoryName) {
+//        return categoryRepository.
+//                findAllByCategoryNameLikeIgnoreCase(categoryName);
+//    }
+//
+//    public Optional<List<CategoryModel>> getByDesc(String description) {
+//        return categoryRepository.
+//                findAllByDescriptionLikeIgnoreCase(description);
+//    }
 
 
 }
