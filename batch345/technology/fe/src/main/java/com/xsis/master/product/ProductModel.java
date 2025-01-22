85c81e2de8dd504da82bbbd4676435dcab4e9545
laqqueta-bootcamp <envyxbot@gmail.com>
2025-01-22 16:34:48 +0700

    + Add model for product

---
+++ batch345/technology/fe/src/main/java/com/xsis/master/product/ProductModel.java
@@ -0,0 +1,109 @@
+package com.xsis.master.product;
+
+import com.xsis.master.variant.VariantModel;
+
+import java.time.LocalDateTime;
+
+public class ProductModel {
+    private int id;
+    private String name;
+    private String image;
+    private double price;
+    private double stock;
+
+    private boolean deleted;
+    private int createBy;
+    private LocalDateTime createDate;
+    private int updateBy;
+    private LocalDateTime updateDate;
+
+    private VariantModel variant;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getStock() {
+        return stock;
+    }
+
+    public void setStock(double stock) {
+        this.stock = stock;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public int getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(int createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(int updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public VariantModel getVariant() {
+        return variant;
+    }
+
+    public void setVariant(VariantModel variant) {
+        this.variant = variant;
+    }
+}
