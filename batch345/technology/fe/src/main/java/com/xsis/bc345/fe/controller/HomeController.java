e39a9018add9b9925f40b0a021e617f42a622d2a
Imi Comp <imicomputer@users.noreply.github.com>
2025-01-24 09:46:03 +0700

    + Trying HTTPSession

--- batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/HomeController.java
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/HomeController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(HttpSession sess) {
         ModelAndView view = new ModelAndView("/index");
+
+        //Session Variables
+        sess.setAttribute("coba", "value dari Home Page");
 
         return view;
     }
