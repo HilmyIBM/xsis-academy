7a4541fa406f9c23196b12192a163134c989ec0d
Alvito <alvito1010@gmail.com>
2025-01-28 09:31:12 +0700

    customer fix + login auth

--- batch345/technology/be/src/main/java/com/xsis/bc345/be/controllers/CustomerController.java
+++ batch345/technology/be/src/main/java/com/xsis/bc345/be/controllers/CustomerController.java
@@ -1,12 +1,18 @@
 package com.xsis.bc345.be.controllers;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,27 +20,102 @@ import com.xsis.bc345.be.models.Customer;
 import com.xsis.bc345.be.services.CustomerService;
 
 @RestController
-@CrossOrigin("*")
-@RequestMapping("api/customer")
+@RequestMapping("/api/customer")
 public class CustomerController {
     @Autowired
     private CustomerService customerSvc;
 
-@GetMapping("")
-    public ResponseEntity<?> getAll(){
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
         try {
-            List<Customer> data = customerSvc.getAll();
+            List<Map<String, Object>> data = customerSvc.getAll();
 
-            if (data.size()>0){
-                return new ResponseEntity<List<Customer>>(data, HttpStatus.OK);
-                
-            } else {
-                return new ResponseEntity<List<Customer>>(data, HttpStatus.NO_CONTENT);
-                
-            }
+            return new ResponseEntity<List<Map<String, Object>>>(
+                data,
+                data.size() > 0 ? HttpStatus.OK : HttpStatus.NO_CONTENT
+            );
         } catch (Exception e) {
-            // TODO: handle exception
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<?> getBy(int id) {
+        try {
+            Optional<Map<String, Object>> data = customerSvc.getBy(id);
+
+            return new ResponseEntity<Map<String, Object>>(
+                data.get(),
+                data.isPresent() ? HttpStatus.OK : HttpStatus.NO_CONTENT
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/filter/{filter}")
+    public ResponseEntity<?> getBy(String filter) {
+        try {
+            List<Map<String, Object>> data = customerSvc.getBy(filter);
+
+            return new ResponseEntity<List<Map<String, Object>>>(
+                data,
+                data.size() > 0 ? HttpStatus.OK : HttpStatus.NO_CONTENT
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<?> getByEmail(@PathVariable String email) {
+        try {
+            Optional<Customer> data = customerSvc.getByEmail(email);
+
+            return new ResponseEntity<Customer>(
+                data.get(),
+                data.isPresent() ? HttpStatus.OK : HttpStatus.NO_CONTENT
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Customer data) {
+        try {
+            return new ResponseEntity<Customer>(customerSvc.create(data), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Customer data) {
+        try {
+            return new ResponseEntity<Customer>(customerSvc.update(data), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}/{userId}")
+    public ResponseEntity<?> delete(
+        @PathVariable int id,
+        @PathVariable int userId
+    ) {
+        try {
+            final Customer data = customerSvc.delete(id, userId);
+
+            if (data.isDeleted()){
+                return new ResponseEntity<Customer> (data, HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<String>("Failed to delete Customer with ID = (" + id + ")", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
---
+++ batch345/technology/fe/src/main/java/com/xsis/bc345/fe/controller/CustomerController.java
@@ -0,0 +1,118 @@
+package com.xsis.bc345.fe.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.xsis.bc345.fe.models.CustomerView;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.server.PathParam;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    //HTTP Client
+    private RestTemplate restTemplate = new RestTemplate();
+    
+    //API URL
+    @Value("${application.api.url}")
+    private String apiUrl;
+
+    @GetMapping("")
+    public ModelAndView index(String filter, HttpSession sess) throws Exception {
+        ModelAndView view = new ModelAndView("/customer/index");
+        ResponseEntity<CustomerView[]> apiResponse = null;
+
+        try {
+            if (filter == null || filter.isBlank()) {
+                apiResponse = restTemplate.getForEntity(apiUrl + "/customer", CustomerView[].class);
+            }
+            else {
+                apiResponse = restTemplate.getForEntity(apiUrl + "/customer/filter/" + filter, CustomerView[].class);
+            }
+
+            if (apiResponse.getStatusCode() == HttpStatus.OK) {
+                view.addObject("customers", apiResponse.getBody());
+            }
+            else {
+                throw new Exception(apiResponse.getStatusCode().toString() + ": " + apiResponse.getBody());
+            }
+        }
+        catch (Exception e) {
+            view.addObject("errorMsg", e.getMessage());
+        }
+
+        return view;
+    }
+
+    @GetMapping("/add")
+    public ModelAndView add() {
+        ModelAndView view = new ModelAndView("/customer/add");
+
+        view.addObject("title", "Add New Customer");
+
+        return view;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@ModelAttribute CustomerView data) {
+        try {
+            ResponseEntity<CustomerView> apiResponse = restTemplate.postForEntity(apiUrl + "/customer/create", data, CustomerView.class);
+            
+            if (apiResponse.getStatusCode() == HttpStatus.CREATED) {
+                return new ResponseEntity<CustomerView>(apiResponse.getBody(), apiResponse.getStatusCode());
+            }
+            else {
+                throw new Exception(apiResponse.getStatusCode().toString() + ": " + apiResponse.getBody());
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable int id) {
+        ModelAndView view = new ModelAndView("/customer/edit");
+
+        view.addObject("title", "view");
+        view.addObject("id", id);
+
+        return view;
+    }
+        
+    @PostMapping("/edit/{id}")
+    public ResponseEntity<?> edit(@ModelAttribute CustomerView data) {
+        ModelAndView view = new ModelAndView("/customer/edit");
+
+        try {
+            view.addObject("title", "view");
+
+            return new ResponseEntity<CustomerView>(new CustomerView(), HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable int id) {
+        ModelAndView view = new ModelAndView("/customer/delete");
+
+        view.addObject("title", "Delete Customer");
+        view.addObject("id", id);
+
+        return view;
+    }
+
+    
+}
